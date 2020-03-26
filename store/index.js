3e6357b7a23c3ed26f3ad57c80cc4e83a39e8c5e
jamesportis <jamesportis@yahoo.com>
2020-03-26 14:14:48 -0700

    new file:   plugins/posts.server.js 	modified:   store/index.js

--- store/index.js
+++ store/index.js
@@ -1,19 +1,27 @@
+const siteURL = "https://css-tricks.com"
+
 export const state = () => ({
     posts: []
 })
 
 export const mutations = {
-    updateValue: (state, posts) => {
-        state.posts = posts
+    updatePosts: (state, posts) => {
+      state.posts = posts
+    },
+    updateTags: (state, tags) => {
+      state.tags = tags
     }
-}
+  }
 
-export const actions = {
-    async getPosts({ state, commit }) {
+  export const actions = {
+    async getPosts({ state, commit, dispatch }) {
       if (state.posts.length) return
+  
       try {
-        let posts = await fetch( `https://css-tricks.com/wp-json/wp/v2/posts?page=1&per_page=20&_embed=1`
+        let posts = await fetch(
+          `${siteURL}/wp-json/wp/v2/posts?page=1&per_page=20&_embed=1`
         ).then(res => res.json())
+  
         posts = posts
           .filter(el => el.status === "publish")
           .map(({ id, slug, title, excerpt, date, tags, content }) => ({
@@ -25,9 +33,33 @@ export const actions = {
             tags,
             content
           }))
+  
         commit("updatePosts", posts)
       } catch (err) {
         console.log(err)
       }
-   }
-}
+    },
+    async getTags({ state, commit }) {
+      if (state.tags.length) return
+  
+      let allTags = state.posts.reduce((acc, item) => {
+        return acc.concat(item.tags)
+      }, [])
+      allTags = allTags.join()
+  
+      try {
+        let tags = await fetch(
+          `${siteURL}/wp-json/wp/v2/tags?page=1&per_page=40&include=${allTags}`
+        ).then(res => res.json())
+  
+        tags = tags.map(({ id, name }) => ({
+          id,
+          name
+        }))
+  
+        commit("updateTags", tags)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  }
